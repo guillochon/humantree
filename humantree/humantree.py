b25e4ee422784a2102749e2320accd0acc7e5233
James Guillochon <guillochon@gmail.com>
2018-06-13 08:20:09 -0400

    adjusting epochs/limit

--- humantree/humantree.py
+++ humantree/humantree.py
@@ -410,7 +410,7 @@ class HumanTree(object):
 
         model.fit(
             self._imgs_train, self._imgs_mask_train, batch_size=8,
-            epochs=20, verbose=1, shuffle=True, validation_split=0.2,
+            epochs=40, verbose=1, shuffle=True, validation_split=0.2,
             callbacks=[model_checkpoint])
 
     def predict_test(self):
