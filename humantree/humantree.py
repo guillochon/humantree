4042dab3a62dd77e9a2c9ac1684f553d8a5dbb0a
James Guillochon <guillochon@gmail.com>
2018-06-18 23:18:27 -0400

    MAINT: numeric changes

--- humantree/humantree.py
+++ humantree/humantree.py
@@ -290,48 +290,57 @@ class HumanTree(object):
         from keras.optimizers import Adam
         from keras.models import Model
         from keras.layers import (Conv2D, Conv2DTranspose, Input, MaxPooling2D,
-                                  concatenate)
+                                  concatenate, Dropout)
 
         inputs = Input((self._SCALED_SIZE, self._SCALED_SIZE, 3))
         # inputs = Input((512, 512, 3))
-        conv1 = Conv2D(32, (3, 3), activation='relu', padding='same')(inputs)
-        conv1 = Conv2D(32, (3, 3), activation='relu', padding='same')(conv1)
+        conv1 = Conv2D(32, (3, 3), activation='relu', padding='same', kernel_initializer='he_normal')(inputs)
+        conv1 = Dropout(0.5)(conv1)
+        conv1 = Conv2D(32, (3, 3), activation='relu', padding='same', kernel_initializer='he_normal')(conv1)
         pool1 = MaxPooling2D(pool_size=(2, 2))(conv1)
 
-        conv2 = Conv2D(64, (3, 3), activation='relu', padding='same')(pool1)
-        conv2 = Conv2D(64, (3, 3), activation='relu', padding='same')(conv2)
+        conv2 = Conv2D(64, (3, 3), activation='relu', padding='same', kernel_initializer='he_normal')(pool1)
+        conv2 = Dropout(0.5)(conv2)
+        conv2 = Conv2D(64, (3, 3), activation='relu', padding='same', kernel_initializer='he_normal')(conv2)
         pool2 = MaxPooling2D(pool_size=(2, 2))(conv2)
 
-        conv3 = Conv2D(128, (3, 3), activation='relu', padding='same')(pool2)
-        conv3 = Conv2D(128, (3, 3), activation='relu', padding='same')(conv3)
+        conv3 = Conv2D(128, (3, 3), activation='relu', padding='same', kernel_initializer='he_normal')(pool2)
+        conv3 = Dropout(0.5)(conv3)
+        conv3 = Conv2D(128, (3, 3), activation='relu', padding='same', kernel_initializer='he_normal')(conv3)
         pool3 = MaxPooling2D(pool_size=(2, 2))(conv3)
 
-        conv4 = Conv2D(256, (3, 3), activation='relu', padding='same')(pool3)
-        conv4 = Conv2D(256, (3, 3), activation='relu', padding='same')(conv4)
+        conv4 = Conv2D(256, (3, 3), activation='relu', padding='same', kernel_initializer='he_normal')(pool3)
+        conv4 = Dropout(0.5)(conv4)
+        conv4 = Conv2D(256, (3, 3), activation='relu', padding='same', kernel_initializer='he_normal')(conv4)
         pool4 = MaxPooling2D(pool_size=(2, 2))(conv4)
 
-        conv5 = Conv2D(512, (3, 3), activation='relu', padding='same')(pool4)
-        conv5 = Conv2D(512, (3, 3), activation='relu', padding='same')(conv5)
+        conv5 = Conv2D(512, (3, 3), activation='relu', padding='same', kernel_initializer='he_normal')(pool4)
+        conv5 = Dropout(0.5)(conv5)
+        conv5 = Conv2D(512, (3, 3), activation='relu', padding='same', kernel_initializer='he_normal')(conv5)
 
         up6 = concatenate([Conv2DTranspose(256, (2, 2), strides=(
             2, 2), padding='same')(conv5), conv4], axis=3)
-        conv6 = Conv2D(256, (3, 3), activation='relu', padding='same')(up6)
-        conv6 = Conv2D(256, (3, 3), activation='relu', padding='same')(conv6)
+        conv6 = Conv2D(256, (3, 3), activation='relu', padding='same', kernel_initializer='he_normal')(up6)
+        conv6 = Dropout(0.5)(conv6)
+        conv6 = Conv2D(256, (3, 3), activation='relu', padding='same', kernel_initializer='he_normal')(conv6)
 
         up7 = concatenate([Conv2DTranspose(128, (2, 2), strides=(
             2, 2), padding='same')(conv6), conv3], axis=3)
-        conv7 = Conv2D(128, (3, 3), activation='relu', padding='same')(up7)
-        conv7 = Conv2D(128, (3, 3), activation='relu', padding='same')(conv7)
+        conv7 = Conv2D(128, (3, 3), activation='relu', padding='same', kernel_initializer='he_normal')(up7)
+        conv7 = Dropout(0.5)(conv7)
+        conv7 = Conv2D(128, (3, 3), activation='relu', padding='same', kernel_initializer='he_normal')(conv7)
 
         up8 = concatenate([Conv2DTranspose(64, (2, 2), strides=(
             2, 2), padding='same')(conv7), conv2], axis=3)
-        conv8 = Conv2D(64, (3, 3), activation='relu', padding='same')(up8)
-        conv8 = Conv2D(64, (3, 3), activation='relu', padding='same')(conv8)
+        conv8 = Conv2D(64, (3, 3), activation='relu', padding='same', kernel_initializer='he_normal')(up8)
+        conv8 = Dropout(0.5)(conv8)
+        conv8 = Conv2D(64, (3, 3), activation='relu', padding='same', kernel_initializer='he_normal')(conv8)
 
         up9 = concatenate([Conv2DTranspose(32, (2, 2), strides=(
             2, 2), padding='same')(conv8), conv1], axis=3)
-        conv9 = Conv2D(32, (3, 3), activation='relu', padding='same')(up9)
-        conv9 = Conv2D(32, (3, 3), activation='relu', padding='same')(conv9)
+        conv9 = Conv2D(32, (3, 3), activation='relu', padding='same', kernel_initializer='he_normal')(up9)
+        conv9 = Dropout(0.5)(conv9)
+        conv9 = Conv2D(32, (3, 3), activation='relu', padding='same', kernel_initializer='he_normal')(conv9)
 
         conv10 = Conv2D(1, (1, 1), activation='sigmoid')(conv9)
 
@@ -339,7 +348,7 @@ class HumanTree(object):
 
         model.summary()
 
-        model.compile(optimizer=Adam(lr=1e-4),
+        model.compile(optimizer=Adam(lr=1e-5),
                       loss='binary_crossentropy',
                       metrics=['binary_crossentropy', 'acc'])
 
@@ -383,7 +392,7 @@ class HumanTree(object):
         """Train DNN for image segmentation."""
         import pickle
         from keras import backend as K
-        from keras.callbacks import ModelCheckpoint
+        from keras.callbacks import ModelCheckpoint, TensorBoard
 
         self.notice('Loading and preprocessing train data...')
 
@@ -399,11 +408,12 @@ class HumanTree(object):
 
         self.notice('Fitting model...')
 
-        tbCallBack = K.callbacks.TensorBoard()
+        tbCallBack = TensorBoard(write_grads=True, batch_size=8)
 
         model.fit(
             self._imgs_train, self._imgs_mask_train, batch_size=8,
-            epochs=40, verbose=1, shuffle=True, validation_split=0.2,
+            epochs=50, verbose=1, shuffle=True, validation_split=0.2,
+            # callbacks=[model_checkpoint])
             callbacks=[model_checkpoint, tbCallBack])
 
     def predict_test(self):
