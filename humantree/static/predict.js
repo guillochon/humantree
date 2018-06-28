76a1f6e4a4ccc3e8de907e58dcd37508c91dbdc5
guillochon <guillochon@gmail.com>
2018-06-28 14:35:35 -0400

    MAINT: links target new tabs

--- humantree/static/predict.js
+++ humantree/static/predict.js
@@ -182,7 +182,7 @@ function metrics(image, head) {
         met_str += 'Estimated house value: $' + String((
             mets['house_value']).toFixed(0)).replace(
             /\B(?=(\d{3})+(?!\d))/g, ",") +
-          ' (via <a href="https://www.zillow.com"><img width=50px src="static/zillow.gif"></a>).<br>';
+          ' (via <a href="https://www.zillow.com" target="_blank"><img width=50px src="static/zillow.gif"></a>).<br>';
         met_str += 'Value increase from present trees: $' + String((
           mets['value_increase']).toFixed(0)).replace(
           /\B(?=(\d{3})+(?!\d))/g, ',') + '.<br>'
